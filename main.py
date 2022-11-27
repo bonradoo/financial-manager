9348575e6b376584b7ee96aadeb617e29bcf6f3d
bonradoo <264061@student.pwr.edu.pl>
2022-11-27 19:07:31 +0100

    basic income/expense functionality; save to file;

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from datetime import date
+import os
 import pandas as pd
 
 #fix pandas highliting
 
 #Type,Shop,Title,Amount
-#inc,NaN,Income,Amount
+#inc,-,Income,Amount
 #exp,Amazon,Jedzenie,Amount
+def clear(): os.system('cls')
 
 def highlight_rows(row):
     value = row.loc['type']
@@ -16,10 +18,19 @@ def highlight_rows(row):
     return ['background-color: {}'.format(color) for r in row]
 
 def addIncome():
-    pass
+    while True:
+        incTitle = input('Income title: ')
+        incAmount = input('Income amount: ')
+        if incAmount.isnumeric(): return['inc', '-', incTitle, incAmount]
+        else: print('Incorrect input. Try again')
 
 def addExpense():
-    pass
+    while True:
+        expTitle = input('Expense title: ')
+        expPlace = input('Expense place: ')
+        expAmount = input('Expense amount: ')
+        if expAmount.isnumeric(): return['exp', expPlace, expTitle, expAmount]
+        else: print('Incorrect input. Try again')
 
 def saveToFile(line):
     thisMonth = date.today().strftime('%B')
@@ -30,13 +41,36 @@ def saveToFile(line):
 def printBalance():
     thisMonth = date.today().strftime('%B')
     filePath = 'logFinance' + str(thisMonth) + '.csv'
-    df = pd.read_csv(r'example.csv', sep=',')
+    df = pd.read_csv(filePath, sep=',')
     df.style.apply(highlight_rows, axis=1)
     print(df)
 
+def budgetMenu():
+    while True:
+        print('1. Add income')
+        print('2. Add expense')
+        print('3. Show balance')
+        print('4. Quit')
+        print('----------------')
+        choice = input('Choice: ')
+        match choice:
+            case '1':
+                clear()
+                saveToFile(addIncome())
+            case '2':
+                clear()
+                saveToFile(addExpense())
+            case '3':
+                clear()
+                printBalance()
+            case '4':
+                quit()
+            case _:
+                print('Incorrect choice')
+        
+
 def main():
-    #saveToFile(['Amazon', 'Food', str(130.12)])
-    printBalance()
+    budgetMenu()
     
 
 if __name__ == '__main__':
