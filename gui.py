25780a82b761fb6018258f47062af07a3534c000
bonradoo <264061@student.pwr.edu.pl>
2023-01-30 22:24:26 +0100

    added basic gui; moved terminal to legacy code;

--- gui.py
+++ gui.py
@@ -2,100 +2,11 @@ import tkinter
 import customtkinter
 from datetime import date
 import os
+import gui_budget, gui_invest
+import tkinter as tk
 
-def isNumber(num):
-    try:
-        float(num)
-        return True
-    except ValueError:
-        return False
-
-def saveToFile(line):
-    print(line)
-    try:
-        # thisMonth = date.today().strftime('%B')
-        # thisYear = date.today().strftime('%Y')
-
-        # filePath = './bin/log/' + str(thisYear)
-        # if not os.path.exists(filePath): os.makedirs(filePath)
-        
-        # filePath = filePath + '/logFinance' + str(thisMonth) + '.txt'
-
-        if line[1] == '': line[1] = '-'
-
-        filePath = 'logs.txt'   #for testing purpose
-        if not os.path.exists(filePath): 
-            with open(filePath, 'w') as f: 
-                f.write('Type,Shop,Title,Amount\n')
-
-        with open(filePath, 'r') as f:
-            if f.readline() == 'Type,Shop,Title,Amount\n':
-                hasHeading = True
-            else:
-                print('No header')
-                hasHeading = False
-
-        with open(filePath, 'a', encoding='utf-8') as f:
-            if hasHeading:
-                f.write(','.join(line) + '\n')
-            else:
-                f.write('Type,Shop,Title,Amount\n')
-                f.write(','.join(line) + '\n')
-            
-    except:
-        print('Error occured while saving the file')
-
-def clearEntry(shop, title, amount):
-    shop.delete()
-    title.delete()
-    amount.delete()
-
-def testo(app):
-    typeVar = tkinter.IntVar()
-    radiobutton_1 = customtkinter.CTkRadioButton(app, text="0", radiobutton_height=15, radiobutton_width=15, border_width_checked=5, width=30,
-                                                variable=typeVar, value=0)
-    radiobutton_1.place(relx=0.13, rely=0.3)
-
-    radiobutton_2 = customtkinter.CTkRadioButton(app, text="1", radiobutton_height=15, radiobutton_width=15, border_width_checked=5, width=30,
-                                                variable=typeVar, value=1)
-    radiobutton_2.place(relx=0.02, rely=0.3)
-    return typeVar.get()
-
-def frame2(app):
-    frame = customtkinter.CTkFrame(app, width=770, height=50, corner_radius=10, bg_color='red')
-    frame.place(relx=0.02, rely=0.5)
-
-    return frame
-
-def addLog(app):
-    frame = customtkinter.CTkFrame(app, width=770, height=50, corner_radius=10)
-    frame.place(relx=0.02, rely=0.01)
-
-    shop = customtkinter.CTkEntry(frame, width=150, placeholder_text="Shop")
-    shop.place(relx=0.33, rely=0.47, anchor=tkinter.CENTER)
-
-    title = customtkinter.CTkEntry(frame, width=150, placeholder_text="Title")
-    title.place(relx=0.53, rely=0.47, anchor=tkinter.CENTER)
-
-    amount = customtkinter.CTkEntry(frame, width=150, placeholder_text="Amount (float with '.')")
-    amount.place(relx=0.73, rely=0.47, anchor=tkinter.CENTER)
 
     
-    typeVar = tkinter.StringVar()
-    radiobutton_1 = customtkinter.CTkRadioButton(frame, text="Income", radiobutton_height=15, radiobutton_width=15, border_width_checked=5, width=30,
-                                                variable=typeVar, value='inc', command=lambda: shop.configure(state='disabled', fg_color='#191922'))
-    radiobutton_1.place(relx=0.13, rely=0.28)
-
-    radiobutton_2 = customtkinter.CTkRadioButton(frame, text="Expense", radiobutton_height=15, radiobutton_width=15, border_width_checked=5, width=30,
-                                                variable=typeVar, value='exp', command=lambda: shop.configure(state='normal', fg_color='#343638'))
-    radiobutton_2.place(relx=0.02, rely=0.28)
-        
-    button = customtkinter.CTkButton(frame, text='Save', width=100, command=lambda:
-                                    [saveToFile([typeVar.get(), shop.get(), title.get(), amount.get()]), 
-                                    shop.delete(0, 'end'), title.delete(0, 'end'), amount.delete(0, 'end')])
-    button.place(relx=0.92, rely=0.47, anchor=tkinter.CENTER)
-
-    return frame
 
     # def printVar():
     #     if (len(shop.get()) and len(title.get()) and len(amount.get()) and len(typeVar.get())) == 0: 
@@ -113,16 +24,25 @@ def start():
     customtkinter.set_appearance_mode('System')
     customtkinter.set_default_color_theme('blue')
     app = customtkinter.CTk()
+    # app.resizable(False, False)
     app.title('Financial Tracker')
     app.geometry('800x600')
 
-    print(testo(app))
-    if testo(app):
-        run = addLog(app)
-    else:
-        run = frame2(app)
-    
-    
+    budgetFrame = customtkinter.CTkFrame(app, width=770, height=500, corner_radius=10, fg_color='red')
+    gui_budget.addLog(budgetFrame)
+
+    investFrame = customtkinter.CTkFrame(app, width=770, height=500, corner_radius=10, fg_color='blue')
+
+    def choice(app):
+        radiobutton_1 = customtkinter.CTkRadioButton(app, text="f2", radiobutton_height=15, radiobutton_width=15, border_width_checked=5, width=30, 
+                                                    command=lambda: [investFrame.place(relx=0.016, rely=0.15), budgetFrame.place_forget()])
+        radiobutton_1.place(relx=0.13, rely=0.3)
+
+        radiobutton_2 = customtkinter.CTkRadioButton(app, text="bud", radiobutton_height=15, radiobutton_width=15, border_width_checked=5, width=30,
+                                                    command=lambda: [budgetFrame.place(relx=0.016, rely=0.15), investFrame.place_forget()])
+        radiobutton_2.place(relx=0.02, rely=0.3)
+
+    choice(app)
 
     app.mainloop()
 
---
+++ gui_budget.py
@@ -0,0 +1,155 @@
+import tkinter
+import customtkinter
+import os
+import pandas as pd
+import pandastable
+
+
+def clearEntry(shop, title, amount):
+    shop.delete()
+    title.delete()
+    amount.delete()
+
+
+def isNumber(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+    
+
+def saveToFile(line):
+    try:
+        # thisMonth = date.today().strftime('%B')
+        # thisYear = date.today().strftime('%Y')
+
+        # filePath = './bin/log/' + str(thisYear)
+        # if not os.path.exists(filePath): os.makedirs(filePath)
+        
+        # filePath = filePath + '/logFinance' + str(thisMonth) + '.txt'
+
+        if line[1] == '': line[1] = '-'
+
+        filePath = 'logs.txt'   #for testing purpose
+        if not os.path.exists(filePath): 
+            with open(filePath, 'w') as f: 
+                f.write('Type,Shop,Title,Amount\n')
+
+        with open(filePath, 'r') as f:
+            if f.readline() == 'Type,Shop,Title,Amount\n':
+                hasHeading = True
+            else:
+                print('No header')
+                hasHeading = False
+
+        with open(filePath, 'a', encoding='utf-8') as f:
+            if hasHeading:
+                f.write(','.join(line) + '\n')
+            else:
+                f.write('Type,Shop,Title,Amount\n')
+                f.write(','.join(line) + '\n')
+            
+    except:
+        print('Error occured while saving the file')
+
+
+def addLog(app):
+    frame = customtkinter.CTkFrame(app, width=740, height=50, corner_radius=10)
+    frame.place(relx=0.02, rely=0.01)
+
+    shop = customtkinter.CTkEntry(frame, width=150, placeholder_text="Shop")
+    shop.place(relx=0.33, rely=0.47, anchor=tkinter.CENTER)
+
+    title = customtkinter.CTkEntry(frame, width=150, placeholder_text="Title")
+    title.place(relx=0.53, rely=0.47, anchor=tkinter.CENTER)
+
+    amount = customtkinter.CTkEntry(frame, width=150, placeholder_text="Amount (float with '.')")
+    amount.place(relx=0.73, rely=0.47, anchor=tkinter.CENTER)
+
+    typeVar = tkinter.StringVar()
+    radiobutton_1 = customtkinter.CTkRadioButton(frame, text="Income", radiobutton_height=15, radiobutton_width=15, border_width_checked=5, width=30,
+                                                variable=typeVar, value='inc', command=lambda: shop.configure(state='disabled', fg_color='#191922'))
+    radiobutton_1.place(relx=0.13, rely=0.28)
+
+    radiobutton_2 = customtkinter.CTkRadioButton(frame, text="Expense", radiobutton_height=15, radiobutton_width=15, border_width_checked=5, width=30,
+                                                variable=typeVar, value='exp', command=lambda: shop.configure(state='normal', fg_color='#343638'))
+    radiobutton_2.place(relx=0.02, rely=0.28)
+        
+    button = customtkinter.CTkButton(frame, text='Save', width=100, command=lambda:
+                                    [saveToFile([typeVar.get(), shop.get(), title.get(), amount.get()]), 
+                                    shop.delete(0, 'end'), title.delete(0, 'end'), amount.delete(0, 'end')])
+    button.place(relx=0.92, rely=0.47, anchor=tkinter.CENTER)
+
+    printBudget(app)
+
+def printBudget(frame):
+    insideFrame = customtkinter.CTkFrame(frame, width=740, height=250, corner_radius=10)
+    insideFrame.place(relx=0.02, rely=0.4)
+
+    textbox = customtkinter.CTkTextbox(insideFrame, width=600, height=200)
+    textbox.place(relx=0.02, rely=0.1)
+
+    # dic = {'name': 'jan pawel ii', 'date': '2137', 'real': 'karol wojtyla'}
+    # df = pd.DataFrame(dic)
+    # textbox.insert('0.3', 'Amount\t\t')
+    # textbox.insert('0.2', 'Title\t\t')
+    # textbox.insert('0.1', 'Shop\t\t')
+    # textbox.insert('0.0', 'Type\t\t')
+
+    # textbox.insert('1.3', '1000\t\t')
+    # textbox.insert('1.2', 'Wypłata\t\t')
+    # textbox.insert('1.1', '-\t\t')
+    # textbox.insert('1.0', 'inc\t\t')
+
+
+
+    def addToDict(sample_dict, key, list_of_values):
+        if key not in sample_dict.keys():
+            sample_dict[key] = list()
+        sample_dict[key].extend(list_of_values)
+        return sample_dict
+
+    filePath = 'logs.txt'
+
+    frames = []
+    with open(filePath, 'r', encoding='utf-8') as file:
+        for line in file: frames.append(line.strip('\n'))
+    elements = [i.split(',') for i in frames]
+
+    res = []
+    for j in range(3):
+        temp = []
+        for i in range(len(elements)):
+            temp.append(elements[i][j])
+        res.append(temp)
+
+    resDict = {}
+    for i in res:
+        addToDict(resDict, i[0], i[1:]) 
+
+    # print(resDict)
+    
+    # df = pd.DataFrame(resDict)
+    # print(df)
+
+    
+
+
+
+    for i in range(5):
+        textbox.insert(str(i) + '.0','\n')
+        for j in range(4):
+            textbox.insert(str(i) + '.' + str(j), 'Amount\t\t')
+    
+    # textbox.insert("0.0", "new text to insert")  # insert at line 0 character 0
+    # text = textbox.get("0.0", "end")  # get text from line 0 character 0 till the end
+
+    textbox.configure(state="disabled") 
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
