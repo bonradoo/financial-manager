e9f378017d0771975f220fcc5cebc6716d1aff72
Konrad Bik <264061@student.pwr.edu.pl>
2023-02-14 15:29:11 +0100

    added record showing; fixed file save;

--- gui_budget.py
+++ gui_budget.py
@@ -31,31 +31,31 @@ def saveToFile(line):
         filePath = filePath + '/' + str(thisMonth) + '.txt'
 
         # Checking formatting
-        if line[0]=='' or line[2]=='':
-            if not line[3].isNumber() or line[3]=='':
+        if line[0]=='' or line[1]=='' or line[2]=='' or line[3]=='':
+            if not line[3].isNumber():
                 print('Unable to save')
                 return
-        if line[1] == '': line[1] = '-'
+            return
+        line[3] = str(line[3]).replace(',', '.')
 
         # Checking if file exists
         if not os.path.exists(filePath): 
             with open(filePath, 'w') as f: 
-                f.write('Type,Shop,Title,Amount\n')
+                f.write('Type,Place,Title,Amount\n')
 
         # Checking if file has heading
         with open(filePath, 'r') as f:
-            if f.readline() == 'Type,Shop,Title,Amount\n': hasHeading = True
+            if f.readline() == 'Type,Place,Title,Amount\n': hasHeading = True
             else: hasHeading = False
 
         with open(filePath, 'a', encoding='utf-8') as f:
             if hasHeading:
                 f.write(','.join(line) + '\n')
             else:
-                f.write('Type,Shop,Title,Amount\n')
+                f.write('Type,Place,Title,Amount\n')
                 f.write(','.join(line) + '\n')   
     except:
         print('Error occured while saving the file')
-
 
 # Create files depending on current year and month
 def createFiles():
@@ -73,8 +73,7 @@ def createFiles():
         # Check if file for the current month already exists
         if not os.path.exists(fp):
             with open(fp, 'w') as file:
-                file.write('Type,Shop,Title,Amount\n')
-
+                file.write('Type,Place,Title,Amount\n')
 
 # Main budget frame
 def addLog(app):
@@ -103,6 +102,7 @@ def addLog(app):
         embFP = filePath
         embFP += monthVal.get()
         printTotals(summaryFrame, embFP)
+        printBudget(summaryFrame, embFP)
         
 
     # Creating CTk assets
@@ -123,45 +123,37 @@ def addLog(app):
         monthCombo.set(getMonth()[0])
 
     # Creating CTk entries
-    shop = customtkinter.CTkEntry(frame, width=230, height=50, placeholder_text="Shop")
+    place = customtkinter.CTkEntry(frame, width=230, height=50, placeholder_text="Place")
     title = customtkinter.CTkEntry(frame, width=230, height=50, placeholder_text="Title")
     amount = customtkinter.CTkEntry(frame, width=230, height=50, placeholder_text="Amount (float with '.')")
 
     # Placing CTk entries
     amount.place(relx=0.5, rely=0.78, anchor=tkinter.CENTER)
-    shop.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+    place.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
     title.place(relx=0.5, rely=0.64, anchor=tkinter.CENTER)
 
-    typeVar = tkinter.StringVar()
+    # Segmented button function
     def segChoice(value):
-        if value == '        Income       ': 
-            shop.delete(0, 'end')
-            shop.configure(state='disabled', fg_color='#191922', placeholder_text='')
-            typeVar.set('inc')
-        elif value == '       Expense        ': 
-            shop.configure(state='normal', fg_color='#343638', placeholder_text='Shop')
-            typeVar.set('exp')
+        if value == '        Income       ': typeVar.set('inc')
+        elif value == '       Expense        ': typeVar.set('exp')
 
-    segmentedButton = customtkinter.CTkSegmentedButton(frame, values=['        Income       ', '       Expense        '], width=230, height=50, corner_radius=10, command=segChoice)
-    segmentedButton.set('Expense')
+    typeVar = tkinter.StringVar()
+    segmentedButton = customtkinter.CTkSegmentedButton(frame, values=['        Income       ', '       Expense        '], 
+                                                       width=230, height=50, corner_radius=10, command=segChoice)
     segmentedButton.place(relx=0.5, rely=0.36, anchor=tkinter.CENTER)
-
-
-    printBudget(summaryFrame)
 
     # Save button action
     def saveAction():
-        saveToFile([typeVar.get(), shop.get(), title.get(), amount.get()])
-        shop.delete(0, 'end')
+        saveToFile([typeVar.get(), place.get(), title.get(), amount.get()])
+        place.delete(0, 'end')
         title.delete(0, 'end')
         amount.delete(0, 'end')
-        printTotals(summaryFrame,filePath + monthVal.get())
-        shop.focus()
-        print('2')
+        printTotals(summaryFrame, filePath + monthVal.get())
+        printBudget(summaryFrame, filePath + monthVal.get())
+        place.focus()
     
     saveButton = customtkinter.CTkButton(frame, text='Save', width=230, height=50, command=saveAction)
     saveButton.place(relx=0.5, rely=0.92, anchor=tkinter.CENTER)
-
 
 # Function for totals
 def printTotals(frame, embFP):
@@ -216,31 +208,54 @@ def printTotals(frame, embFP):
     totalBalValue = customtkinter.CTkLabel(balFrame, width=50, height=30, text=tbv.get()+' PLN', font=('Arial', 12))
     totalBalValue.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
 
-
 # Fuction that prints table of expenses and income
-def printBudget(frame):
-    def addToDict(sample_dict, key, list_of_values):
-        if key not in sample_dict.keys():
-            sample_dict[key] = list()
-        sample_dict[key].extend(list_of_values)
-        return sample_dict
+def printBudget(frame, embFP):
+    recordsFrame = customtkinter.CTkScrollableFrame(frame, width=322, height=397)
+    graphFrame = customtkinter.CTkFrame(frame, width=340, height=410)
 
-    filePath = 'logs.txt'
+    recordsFrame.place(relx=0.743, rely=0.56, anchor=tkinter.CENTER)
+    graphFrame.place(relx=0.25, rely=0.56, anchor=tkinter.CENTER)
+
     frames = []
-    with open(filePath, 'r', encoding='utf-8') as file:
+    with open(embFP, 'r', encoding='utf-8') as file:
         for line in file: frames.append(line.strip('\n'))
     elements = [i.split(',') for i in frames]
+    elements = elements[1:]
+    elements.reverse()
+    
+    # Creating CTk assets
+    titleFrame = customtkinter.CTkFrame(recordsFrame, width=310, height=40, corner_radius=10)
+    headFont = customtkinter.CTkFont(family='Arial', size=16, weight='bold')
+    placeLabel = customtkinter.CTkLabel(titleFrame, text='Place', font=headFont)
+    titleLabel = customtkinter.CTkLabel(titleFrame, text='Title', font=headFont)
+    amountLabel = customtkinter.CTkLabel(titleFrame, text='Amount', font=headFont)
+    
+    # Placing CTk assets inside title frame
+    placeLabel.place(relx=0.1, rely=0.5, anchor=tkinter.CENTER)
+    titleLabel.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+    amountLabel.place(relx=0.8, rely=0.5, anchor=tkinter.CENTER)
 
-    res = []
-    for j in range(3):
-        temp = []
-        for i in range(len(elements)):
-            temp.append(elements[i][j])
-        res.append(temp)
+    # Packing title frame
+    titleFrame.pack()
 
-    resDict = {}
-    for i in res:
-        addToDict(resDict, i[0], i[1:])
+    # Making a list of records
+    for record in range(len(elements)):
+        i = record
+        if elements[i][0] == 'inc': record = customtkinter.CTkFrame(recordsFrame, width=310, height=32, fg_color='#90c695')
+        elif elements[i][0] == 'exp': record = customtkinter.CTkFrame(recordsFrame, width=310, height=32, fg_color='#ff9478')
+
+        # Creating CTk title assets
+        plaL = customtkinter.CTkLabel(record, text=str(elements[i][1]), text_color='black')
+        titL = customtkinter.CTkLabel(record, text=str(elements[i][2]), text_color='black')
+        amtL = customtkinter.CTkLabel(record, text=str(elements[i][3]), text_color='black')
+
+        # Placing record elements
+        plaL.place(relx=0.1, rely=0.5, anchor=tkinter.CENTER)
+        titL.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        amtL.place(relx=0.8, rely=0.5, anchor=tkinter.CENTER)
+
+        # Packing everything
+        record.pack(pady=3)
 
 if __name__ == '__main__':
     pass
