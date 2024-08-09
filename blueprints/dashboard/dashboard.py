32367740e34c54dd9185d2119992ae5af39ac556
bonradoo <264061@student.pwr.edu.pl>
2024-08-09 07:27:19 +0200

    Changes to structure; Changes to the idea behind the manager;

--- blueprints/dashboard/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash, session, jsonify
-
-dashboard_bp = Blueprint('dashboard', __name__, template_folder='templates', static_folder='static')
-
-@dashboard_bp.route('/')
-def dashboard():
-    return render_template('dashboard.html')
--- legacy_code/ctk_legacy/main_gui.py
+++
@@ -1,8 +0,0 @@
-import gui
-
-def main():
-    # Run main gui application
-    gui.start()
-
-if __name__ == '__main__':
-    main()
